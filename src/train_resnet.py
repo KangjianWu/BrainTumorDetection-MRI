d201b3dbfcf52a8cb74cbd416aa3590d2e17d1ed
KangjianWu <124107340+KangjianWu@users.noreply.github.com>
2024-06-21 14:53:06 -0400

    Update train_resnet.py

--- src/train_resnet.py
+++ src/train_resnet.py
@@ -4,34 +4,32 @@ from sklearn.model_selection import train_test_split
 from resnet_model import build_resnet_model
 import matplotlib.pyplot as plt
 
-# 加载预处理后的数据
 train_images = np.load('../data/processed/train_images.npy')
 train_labels = np.load('../data/processed/train_labels.npy')
 test_images = np.load('../data/processed/test_images.npy')
 test_labels = np.load('../data/processed/test_labels.npy')
 
-# 划分训练集和验证集
 X_train, X_val, y_train, y_val = train_test_split(train_images, train_labels, test_size=0.2, random_state=42)
 
-# 构建并训练模型
+# Build and train the model
 input_shape = (224, 224, 3)
-num_classes = 4  # 四个类别：glioma, meningioma, notumor, pituitary
+num_classes = 4  # four types：glioma, meningioma, notumor, pituitary
 model = build_resnet_model(input_shape, num_classes)
 
-# 设置训练参数
+# Setting training parameters
 epochs = 10
 batch_size = 32
 
-# 训练模型并保存历史
+# Train the model and save the history
 history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=epochs, batch_size=batch_size)
 
-# 保存训练好的模型
+# Save the trained model
 model.save('../models/resnet/resnet_model.h5')
 
-# 保存训练历史
+# Preservation of training history
 np.save('../models/resnet/resnet_history.npy', history.history)
 
-# 可视化训练历史
+# Visualization Training History
 plt.plot(history.history['accuracy'], label='Train Accuracy')
 plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
 plt.xlabel('Epoch')
